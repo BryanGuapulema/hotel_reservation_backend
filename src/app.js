cf37b107164035a1a6e3775095011c2fd764ac58
BryanGuapulema <bryan19012003@gmail.com>
2025-09-01 00:29:49 -0500

    fix empty route bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { corsMiddleware } from './middlewares/corsMiddleware.js'
-import { homeRouter } from './routes/home.routes.js'
+import { homeRouter } from './routes/homeRoutes.js'
 
 const PORT = process.env.PORT ?? 1234
 
@@ -9,7 +9,7 @@ app.disable('x-powered-by')
 app.use(corsMiddleware())
 app.use(express.json())
 
-app.get('/', homeRouter)
+app.use('/', homeRouter)
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
