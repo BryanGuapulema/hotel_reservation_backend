a3d5872cdd38171f2163fb6017aa9601d31de897
BryanGuapulema <bryan19012003@gmail.com>
2025-08-31 23:59:15 -0500

    cors middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import { corsMiddleware } from './middlewares/corsMiddleware.js'
 import { homeRouter } from './routes/home.routes.js'
 
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
 app.disable('x-powered-by')
+app.use(corsMiddleware())
 app.use(express.json())
 
 app.get('/', homeRouter)
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -5,3 +5,11 @@ export const homeRouter = Router()
 homeRouter.get('/', (req, res) => {
   res.json({ message: 'home' })
 })
+
+homeRouter.get('/login', (req, res) => {
+  res.json({ message: 'login' })
+})
+
+homeRouter.get('/register', (req, res) => {
+  res.json({ message: 'register' })
+})
